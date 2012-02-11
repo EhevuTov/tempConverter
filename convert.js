13056d81e6fcb5798dcbcb4396d4fdfe16be3287
James Gosnell <jamesgosnell@gmail.com>
2012-02-11 03:17:51 -0600

    INITIAL commit of the conversion program

---
+++ convert.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var arg = require( 'commander' );
+
+
+function f2c( f ) {
+  return 5/9*(f-32)
+}
+
+function c2f( c ) {
+  return 9/5*c+32
+}
+
+arg
+  .version( '0.0.1' )
+  .usage( "<conversion> <temperature>" )
+
+// a new block for each function
+arg
+  .command('c2f <temp>')
+  .description('convert Celsius to Fehrenheit')
+  .action(function(temp){
+    console.log(c2f(temp));
+  });
+arg
+  .command('f2c <temp>')
+  .description('convert Fehrenheit to Celsius')
+  .action(function(temp){
+    console.log(f2c(temp));
+  });
+
+arg.parse(process.argv)
